e5a43804c5f5319b774fc9cddbeeed19876850b6
chris241 <chrisleon325@gmail.com>
2019-08-26 14:12:18 +0300

    maj readme

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
-var p = document.querySelectorAll(".text-muted");
-console.log(p);
+let e = document.querySelector('footer');
+e.addEventListener('click',(function(event){
+let i = 1;
+console.log("clique", + i);
+i++; 
+}));
 
+let navParent = document.getElementsByClassName('navbar-toggler')[0];
+let navCollapse = document.getElementById('navbarHeader');
+navParent.addEventListener('click',function(){
+    navCollapse.classList.toggle('collapse');
+});
+
+let a = document.getElementsByClassName("btn btn-sm btn-outline-secondary");
+console.log(a);
