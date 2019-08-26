0c7416ff9c9911a641cae9b24fb6ece922dc8512
chris241 <chrisleon325@gmail.com>
2019-08-26 15:13:29 +0300

    maj v7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// fonctionnalité n°1
 let e = document.querySelector('footer');
 e.addEventListener('click',(function(event){
 let i = 1;
@@ -6,34 +6,38 @@ console.log("clique", + i);
 i++; 
 }));
 
+//fonctionnalité n°2
 let navParent = document.getElementsByClassName('navbar-toggler')[0];
 let navCollapse = document.getElementById('navbarHeader');
 navParent.addEventListener('click',function(){
     navCollapse.classList.toggle('collapse');
 });
 
-let textCard1 = document.getElementsByClassName('card-text')[0];
-let editCard1 = document.querySelector('button.btn.btn-outline-secondary')[0];
+//fonctionnalité n°3
+let tCard1 = document.getElementsByClassName('card-text')[0];
+let eCard1 = document.querySelector('button.btn.btn-outline-secondary')[0];
 console.log(textCard1.innerHTML);
 addEventListener('click',function(){
-    if(textCard1.style.color === "red"){
-        textCard1.style.color = "";
+    if(tCard1.style.color === "red"){
+        tCard1.style.color = "";
     }
     else{
-        textCard1.style.color = "red";
+        tCard1.style.color = "red";
     }
 });
 
 
-
-let textCard2 = document.getElementsByClassName('card-text')[1];
-let editCard2 = document.querySelector('button.btn.btn-outline-secondary')[1];
-console.log(textCard2.innerHTML);
+//fonctionnalité n°4
+let tCard2 = document.getElementsByClassName('card-text')[1];
+let eCard2 = document.querySelector('button.btn.btn-outline-secondary')[1];
+console.log(tCard2.innerHTML);
 addEventListener('click',function(){
-    if(textCard2.style.color === "green"){
-        textCard2.style.color = "";
+    if(tCard2.style.color === "green"){
+        tCard2.style.color = "";
     }
     else{
-        textCard2.style.color = "green";
+        tCard2.style.color = "green";
     }
 });
+
+
