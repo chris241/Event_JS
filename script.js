474cfde0541309f35e60b18d6d9e8d83bef1a84c
chris241 <chrisleon325@gmail.com>
2019-08-26 14:46:54 +0300

    maj v2

--- script.js
+++ script.js
@@ -12,5 +12,28 @@ navParent.addEventListener('click',function(){
     navCollapse.classList.toggle('collapse');
 });
 
-let a = document.getElementsByClassName("btn btn-sm btn-outline-secondary");
-console.log(a);
+let textCard1 = document.getElementsByClassName('card-text')[0];
+let editCard1 = document.querySelector('div.btn-group button.btn.btn-outline-secondary')[0];
+console.log(textCard1.innerHTML);
+addEventListener('click',function(){
+    if(textCard1.style.color === "red"){
+        textCard1.style.color = "";
+    }
+    else{
+        textCard1.style.color = "red";
+    }
+});
+
+
+
+let textCard2 = document.getElementsByClassName('card-text')[1];
+let editCard2 = document.querySelector('div.btn-group button.btn.btn-outline-secondary')[1];
+console.log(textCard2.innerHTML);
+addEventListener('click',function(){
+    if(textCard2.style.color === "green"){
+        textCard2.style.color = "";
+    }
+    else{
+        textCard2.style.color = "green";
+    }
+});
